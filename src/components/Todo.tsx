df35c303d8b0c5163c23cb1f7aaa66aeb8014304
gizo-fury <nirlaj6.com>
2025-04-24 02:19:00 +0530

    linting error fix/4

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Input, Button } from './FormControl';
+import Button from './FormControl';
+import { Input } from './FormControl';
 
 interface TodoItem {
   id: string;
