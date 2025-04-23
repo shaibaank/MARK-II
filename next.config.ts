d396c8281ba845b91cd6fe8b1c75c0598fe1b6fc
gizo-fury <nirlaj6.com>
2025-04-24 01:28:53 +0530

    Restore project to original state with Todo component fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true
-  }
+  /* config options here */
 };
 
 export default nextConfig;
